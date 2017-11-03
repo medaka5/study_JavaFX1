bf02c4893bdc9a44fdc7598928d6ffd892b9afe6
Hamano Medaka <hamano.medaka@gmail.com>
2017-11-03 20:01:01 +0900

    use FlowPane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,17 +17,19 @@ public class Main extends Application {
         // Stage : Window全体
         // Scene : Stageの中。paneが必要
         // Pane  : Layout的なもの？
-        BorderPane pane = new BorderPane();
+        //BorderPane pane = new BorderPane();
+        FlowPane pane = new FlowPane();
         Scene scene = new Scene(pane, 320, 240);
         primaryStage.setScene(scene);
 
-
         // 文字列表示
         Label label = new Label("Hello");
-        pane.setCenter(label);
-
+//        pane.setCenter(label);
+        pane.getChildren().add(label);
 
         primaryStage.show();
+
+
     }
 
 
