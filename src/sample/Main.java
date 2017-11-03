1abda765b60f3cacd060bd48c446b63ee70fdf22
Hamano Medaka <hamano.medaka@gmail.com>
2017-11-03 20:07:41 +0900

    add button, textfield

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,7 +2,9 @@ package sample;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -14,18 +16,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        // Stage : Window全体
-        // Scene : Stageの中。paneが必要
-        // Pane  : Layout的なもの？
-        //BorderPane pane = new BorderPane();
-        FlowPane pane = new FlowPane();
+
+        BorderPane pane = new BorderPane();
         Scene scene = new Scene(pane, 320, 240);
         primaryStage.setScene(scene);
 
         // 文字列表示
         Label label = new Label("Hello");
-//        pane.setCenter(label);
-        pane.getChildren().add(label);
+        TextField field = new TextField();
+        Button button = new Button("Click Me!");
+        pane.setTop(label);
+        pane.setCenter(field);
+        pane.setBottom(button);
 
         primaryStage.show();
 
